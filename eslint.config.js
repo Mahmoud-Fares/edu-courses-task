0e2597edd56a6973e5ecef3616a0c2e0447abbc7
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-05-04 19:12:12 +0300

    dev-init

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,62 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import checkFile from 'eslint-plugin-check-file';
+import { projectStructurePlugin } from 'eslint-plugin-project-structure';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
+export default {
+   ignores: ['dist'],
+   files: ['**/*.{ts,tsx}'],
+   languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    plugins: {
+      parser: tseslint.parser,
+      parserOptions: {
+         projectService: true,
+      },
+   },
+   plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'check-file': checkFile,
+      'react-compiler': reactCompiler,
+      'project-structure': projectStructurePlugin,
+   },
+   settings: {
+      'project-structure/independent-modules-config-path':
+         'independentModules.json',
+   },
+   rules: {
+      // react-compiler
+      'react-compiler/react-compiler': 'error',
+      // custom rules
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'error',
+
+      'check-file/filename-naming-convention': [
+         'error',
+         {
+            '**/*.{ts,tsx}': 'KEBAB_CASE',
+         },
+         {
+            ignoreMiddleExtensions: true,
+         },
       ],
-    },
-  },
-)
+      'check-file/folder-naming-convention': [
+         'error',
+         {
+            'src/**': 'KEBAB_CASE',
+         },
+      ],
+      'react-refresh/only-export-components': [
+         'warn',
+         { allowConstantExport: true },
+      ],
+      // project-structure
+      'project-structure/independent-modules': 'error',
+      ...reactHooks.configs.recommended.rules,
+   },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [
+      react({
+         babel: {
+            plugins: [['babel-plugin-react-compiler']],
+         },
+      }),
+   ],
 
-  server: {
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+
+   server: {
       port: 7000,
    },
-})
+});
